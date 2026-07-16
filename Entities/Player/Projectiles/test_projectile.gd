71366ec8d54047c9b907597b06a452f9d58178d1
ezra994 <ezra.z.moeller@gmail.com>
2026-07-16 07:14:54 -0500

    Last time before i take a break

--- Entities/Player/Projectiles/test_projectile.gd
+++ Entities/Player/Projectiles/test_projectile.gd
@@ -6,5 +6,6 @@ var speed: float = 3.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_central_force(Vector3.FORWARD * speed)
-	await get_tree().create_timer(3).timeout
-	queue_free()
+	if is_multiplayer_authority():
+		await get_tree().create_timer(3).timeout
+		queue_free()
